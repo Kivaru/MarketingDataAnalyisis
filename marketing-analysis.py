ceccf5524e865d84c4eb72eecd7cf6fcc1125513
Kivaru <kivarugodfrey@gmail.com>
2024-09-03 12:26:35 +0300

    Analysis is done by adding numerical-categorical analysis and categorical-categorical analysis

--- marketing-analysis.py
+++ marketing-analysis.py
@@ -1,14 +1,14 @@
-import matplotlib.pyplot as plt
-import seaborn as sb
-import numpy as np
-import pandas as pd
+# import matplotlib.pyplot as plt
+# import seaborn as sb
+# import numpy as np
+# import pandas as pd
 
 # Step 1
 # read the data set of "marketing-data"
 # When importing your data, skip the first two rows to overcome the skewed rows.
 # This will ensure that your column names are populated correctly.
 # data = pd.read_csv("marketing-data.csv", skiprows=2)
-data = pd.read_csv("marketing-data.csv")
+# data = pd.read_csv("marketing-data.csv")
 
 # print the data
 # print(data.head())
@@ -55,5 +55,40 @@ data = pd.read_csv("marketing-data.csv")
 # sb.pairplot(data=data, vars=['age', 'balance', 'duration'])
 # plt.show()
 
+# by plotting correlation matrix
+# A correlation matrix is used to see the correlation between different variables.
+# The correlation coefficient determines how two variables are correlated.
+# The below table shows the correlation between duration, age, and balance.
+# Correlation tells you how one variable affects the other.
+# This helps us determine how changes in one variable will also cause
+# a change in the other.
 
+# creating a matrix of age, balance and duration as rows and columns
+# data[['age', 'balance', 'duration']].corr()
+# # plot the correlation matrix of age, balance and duration in data frame
+# sb.heatmap(data[['age', 'balance', 'duration']].corr(), annot=True, cmap='Reds')
+# plt.show()
 
+# Step 5b : Numeric-Categorical Analysis
+# When one variable is of numeric type, and another is a categorical variable,
+# you perform numeric-categorical analysis.
+
+# group by marital status to find mean/median balance
+# print(data.groupby('marital')['balance'].mean())
+# print(data.groupby('marital')['balance'].median())
+
+# You can also plot the box plot of marital vs balance.
+# A boxplot will show you the range of values that fall under a certain category
+# sb.boxplot(x=data.marital, y=data.balance)
+# plt.show()
+
+# Step 5c : Categorical-Categorical Analysis
+# When both the variables contain categorical data,
+# you perform categorical-categorical analysis.
+# creating success rate of numerical data type where is_success "yes" = 1, "no" = 0
+# data["is_success"] = np.where(data.is_success == "yes", 1, 0)
+# print(data.is_success.value_counts())
+
+# plot the bar graph of marital status and average value of success rate
+# data.groupby('marital')['is_success'].mean().plot.bar()
+# plt.show()
